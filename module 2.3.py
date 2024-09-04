fbdd8ccb0193ec535007bd784f9847f8b79036c1
Smetanka2002 <needfrig@gmail.com>
2024-09-05 00:38:12 +0300

    Add files via upload

---
+++ module 2.3.py
@@ -0,0 +1,18 @@
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while len(my_list) > 0:
+
+    if my_list[a] < 0:
+        break
+    elif my_list[a] == 0:
+        a = a + 1
+    else:
+
+        print(my_list[a])
+        a = a + 1
+
+
+
+
+
